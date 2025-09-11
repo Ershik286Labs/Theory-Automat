a66343451aea3d96755f477899fa56edb4b58958
Ershik286Labs <ershov.ershik286@gmail.com>
2025-09-11 13:16:49 +0300

    Update Map.cs

--- TeoryLab1/TeoryLab1/Map.cs
+++ TeoryLab1/TeoryLab1/Map.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +9,6 @@ namespace TeoryLab1 {
     internal class Map {
 
         public const int distance = 30;
-
+        public const int sizeMassiveHorse = 10;
     }
 }
