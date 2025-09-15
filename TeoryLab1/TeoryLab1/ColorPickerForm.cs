fd8129f9eb509c59a80b49b51561ddc20fd5b64d
Ershik286Labs <ershov.ershik286@gmail.com>
2025-09-15 19:31:14 +0300

    Add files via upload

---
+++ TeoryLab1/TeoryLab1/ColorPickerForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TeoryLab1 {
+    public partial class ColorPickerForm : Form {
+        public Color SelectedColor { get; private set; } = Color.Black;
+
+        public ColorPickerForm() {
+            InitializeComponent();
+            InitializeColorButtons();
+        }
+
+        private void InitializeColorButtons() {
+            this.Text = "Выберите цвет";
+            this.Size = new Size(375, 250);
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.StartPosition = FormStartPosition.CenterParent;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+
+            int buttonSize = 40;
+            int margin = 10;
+            int buttonsPerRow = 7;
+
+            for (int i = 0; i < Program.horseColor.Length; i++) {
+                var colorButton = new Button {
+                    BackColor = Program.horseColor[i],
+                    Size = new Size(buttonSize, buttonSize),
+                    Location = new Point(
+                        margin + (i % buttonsPerRow) * (buttonSize + margin),
+                        margin + (i / buttonsPerRow) * (buttonSize + margin)
+                    ),
+                    Tag = Program.horseColor[i],
+                    FlatStyle = FlatStyle.Flat
+                };
+
+                colorButton.FlatAppearance.BorderSize = 2;
+                colorButton.FlatAppearance.BorderColor = Color.Black;
+                colorButton.Click += ColorButton_Click;
+
+                this.Controls.Add(colorButton);
+            }
+
+            // Кнопка отмены
+            var cancelButton = new Button {
+                Text = "Отмена",
+                Size = new Size(100, 30),
+                Location = new Point(125, 180)
+            };
+            cancelButton.Click += (s, e) => { this.DialogResult = DialogResult.Cancel; this.Close(); };
+
+            this.Controls.Add(cancelButton);
+        }
+
+        private void ColorButton_Click(object sender, EventArgs e) {
+            if (sender is Button button && button.Tag is Color color) {
+                SelectedColor = color;
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+        }
+
+        private void ColorPickerForm_Load(object sender, EventArgs e) {
+            // Пустая реализация, если не нужна
+        }
+    }
+}
---
+++ TeoryLab1/TeoryLab1/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Media;
+using System.Linq;
+using System.Timers;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeoryLab1 {
+    internal static class Program {
+        public static System.Timers.Timer aTimer;
+        public static bool timerIsRunning = false; //таймер сетнут
+        public static int intervalTimer = 1000;
+        public static Form1 mainForm;
+        private const int move = 1;
+
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            mainForm = new Form1();
+
+            generationMassiveHorse();
+
+            Application.Run(mainForm);
+
+            aTimer?.Stop();
+            aTimer?.Dispose();
+        }
+
+        public static void SetTimer() {
+            // Настраиваем таймер (если он еще не создан)
+            if (aTimer == null) {
+                aTimer = new System.Timers.Timer(intervalTimer);
+                aTimer.Elapsed += OnTimedEvent;
+                aTimer.AutoReset = true;
+            }
+            timerIsRunning = true;
+            aTimer.Enabled = true;
+            mainForm.checkTrackBar();
+        }
+
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e) {
+            mainForm.Invoke(new Action(() => {
+                Random rnd = new Random();
+                // Проверяем, что в списке есть лошади
+                if (mainForm.horses.Length > 0) {
+                    int numberMovingHorse = rnd.Next(0, mainForm.horses.Length);
+                    mainForm.horses[numberMovingHorse].moving(move, ref mainForm.horses, numberMovingHorse);
+                    mainForm.Invalidate();
+                }
+            }));
+        }
+
+        public static void toogleTimer() {
+            if (aTimer != null) {
+
+                if (aTimer.Enabled)
+                    aTimer.Stop();
+                else aTimer.Start();
+            }
+        }
+
+        public static Color[] horseColor = new Color[14]{
+                Color.DarkGreen,
+                Color.Gray,
+                Color.DeepPink,
+                Color.Coral,
+                Color.DarkRed,
+                Color.Red,
+                Color.Orange,
+                Color.Yellow,
+                Color.Green,
+                Color.Cyan,
+                Color.LightBlue,
+                Color.Blue,
+                Color.Purple,
+                Color.Pink
+            };
+
+        public static void generationMassiveHorse() {
+            string[] horseNames = new string[]{
+                "Звёзда",
+                "Альбус",
+                "Гром",
+                "Изумруд",
+                "Багира",
+                "Скиф",
+                "Валькирия",
+                "Эклипс",
+                "Сапфир",
+                "Буян",
+                "Церера",
+                "Атака",
+                "Лира",
+                "Одиссей",
+                "Лаванда",
+                "Борей",
+                "Грация",
+                "Барон",
+                "Секвойя",
+                "Заря"
+            };
+
+            Random rnd = new Random();
+
+            // Создаем новых лошадей
+            for (int i = 0; i < Map.sizeMassiveHorse; i++) {
+                int numberName = rnd.Next(0, horseNames.Length);
+                mainForm.horses[i] = new Horse(horseColor[i], i, horseNames[numberName]);
+            }
+        }
+    }
+}
